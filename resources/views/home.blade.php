70990b08083e897f9e90878b4dafccd9f2ed9d3a
daffamachfud <daffamachfud@gmail.com>
2020-09-08 20:51:37 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+ 
+    <!-- Custom styles for this template -->
+     <link rel="stylesheet" type="text/css" href="/css/main.css">
+    
+   	<title>@yield('title')</title>
+</head>
+<body class="text-center">
+
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">DR</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active " href="/">Home</a>
+            <a class="nav-link" href="/product">Products</a>
+           <a class="nav-link" href="#">Contact</a>
+           
+          </nav>
+        </div>
+      </header>
+
+     
+
+      
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Hello,<br>My name is Daffa Radifanka.</h1>
+        <p class="lead">I’m a Web and Mobile Developer</p>
+        <p class="lead">
+          <a href="/product" class="btn btn-lg btn-secondary">See my products</a>
+        </p>
+      </main>
+
+	<footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>&copy; Copyright 2020 - Daffa Radifanka</a></p>
+        </div>
+      </footer>
+    </div>
+
+
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="/js/.js"></script>
+
+</body>
+</html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,78 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+ 
+    <!-- Custom styles for this template -->
+     <link rel="stylesheet" type="text/css" href="/css/album.css">
+  </head>
+
+  <body>
+  <div class="alert alert-dark" role="alert">
+  Back to <a href="/" class="alert-link">Home</a>.
+</div>
+
+    <main role="main" id="main-section">
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">My Products</h1>
+          <p class="lead text-muted">Here are some of the products that I have developed, including several products in the form of web and mobile applications</p>
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-4">
+              <a href="/">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="image/logosiakang.png" alt="siakang">
+              </div>
+              </a>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top"  src="image/logohijabfest.png" alt="Hijabfest">
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top"  src="image/logodeka.png" alt="DEKa">
+          
+              </div>
+            </div>
+  
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top"  src="image/logokuyhijrah.png" alt="DEKa">
+              </div>
+            </div>
+ 
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    <div class="alert alert-dark" role="alert">
+  Back to <a href="/" class="alert-link">Home</a>.
+</div>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="/js/.js"></script>
+  </body>
+</html>
